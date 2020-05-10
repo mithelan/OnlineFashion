488b97c42930d2bc5b40bbefb10f4d1cf2f807e3
Narthanan <narthi1234@hotmail.com>
2020-05-10 22:41:09 +0530

    check karooo

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
-import React from 'react';
-import Cart from './compound/Cart';
-import './App.css';
-import Home from './compound/Home'
+import React from "react";
+import Cart from "./compound/Cart";
+import "./App.css";
+import Home from "./compound/Home";
 import Homep from "./compound/Homepage";
-import {Provider} from 'react-redux';
+import { Provider } from "react-redux";
 import store from "./store";
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Checkout from "./compound/Checkout";
-
-
-
-
+//narthi
+import CreateProducts from "./component/stockmanager/create-product.component";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomeStock from "./component/stockmanager/homeStock";
+//end of narthi
 
 function App() {
-    return (
-        <Provider store={store}>
-            <div className='App'>
-<BrowserRouter>
-
-    <Home/>
-    <Switch>
-    <Route exact path ='/' component={Homep}/>
-
-    <Route path ='/cart' component={Cart}/>
-        <Route path ='/Checkout' component={Checkout}/>
-</Switch>
-
-
-
-</BrowserRouter>
-
-            </div>
-
-        </Provider>
-
-
-    );
-
-};
-
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          <Home />
+          <Switch>
+            <Route exact path="/" component={Homep} />
+            <Route path="/stockmanager" component={HomeStock} />
+            <Route path="/addStock" component={CreateProducts} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/Checkout" component={Checkout} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </Provider>
+  );
+}
 
 export default App;
