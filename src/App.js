cdfc92e2c51b45e50018538112790372e42921b4
Narthanan <narthi1234@hotmail.com>
2020-05-14 22:51:10 +0530

    patta

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Cart from "./compound/Cart";
 import "./App.css";
 import Home from "./compound/Home";
@@ -13,6 +13,7 @@ import Contactus from "./compound/Contactus";
 import ProductDisplay from "./compound/ProductDisplay";
 //narthi
 import CreateProducts from "./component/stockmanager/create-product.component";
+import EditProduct from "./component/stockmanager/edit-product.component";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomeStock from "./component/stockmanager/homeStock";
 import Register from "./Justu/Register";
@@ -24,7 +25,6 @@ import UserContext from "./Justu/UserContext";
 //end of narthi
 
 export default function App() {
-
   const [userData, setUserData] = useState({
     token: undefined,
     user: undefined,
@@ -38,9 +38,9 @@ export default function App() {
         token = "";
       }
       const tokenRes = await Axios.post(
-          "http://localhost:5000/customers/tokenIsValid",
-          null,
-          { headers: { "x-auth-token": token } }
+        "http://localhost:5000/customers/tokenIsValid",
+        null,
+        { headers: { "x-auth-token": token } }
       );
       if (tokenRes.data) {
         const userRes = await Axios.get("http://localhost:5000/customers/", {
@@ -56,37 +56,30 @@ export default function App() {
     checkLoggedIn();
   }, []);
 
-
-
   return (
     <Provider store={store}>
       <div className="App">
         <BrowserRouter>
           <UserContext.Provider value={{ userData, setUserData }}>
-          <Home />
-          <Switch>
-            <Route exact path="/" component={ProductDisplay} />
-
-            //User
-            <Route path="/Login" component={Loginn} />
-            <Route path="/Register" component={Register} />
-            <Route path="/Profile" component={Profile} />
-
-            <Route path="/stockmanager" component={HomeStock} />
-            <Route path="/addStock" component={CreateProducts} />
-
-            <Route path="/Contactus" component={Contactus} />
-            <Route path="/addStock" component={CreateProducts} />
-            <Route path="/cart" component={Cart} />
-            <Route path="/Checkout" component={Checkout} />
-
-            <Route path="/products/:productsId" component={ProductDetail} />
-          </Switch>
+            <Home />
+            <Switch>
+              <Route exact path="/" component={ProductDisplay} />
+              //User
+              <Route path="/Login" component={Loginn} />
+              <Route path="/Register" component={Register} />
+              <Route path="/Profile" component={Profile} />
+              <Route path="/stockmanager" component={HomeStock} />
+              <Route path="/addStock" component={CreateProducts} />
+              <Route path="/editStock/:id" component={EditProduct} />
+              <Route path="/Contactus" component={Contactus} />
+              <Route path="/addStock" component={CreateProducts} />
+              <Route path="/cart" component={Cart} />
+              <Route path="/Checkout" component={Checkout} />
+              <Route path="/products/:productsId" component={ProductDetail} />
+            </Switch>
           </UserContext.Provider>
         </BrowserRouter>
       </div>
     </Provider>
   );
 }
-
-
