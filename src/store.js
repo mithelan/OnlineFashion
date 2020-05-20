e32114eaa50341efaff78d6a8441f2163ed4cced
Narthanan <narthi1234@hotmail.com>
2020-05-20 22:12:24 +0530

    check

--- src/store.js
+++ src/store.js
@@ -1,27 +1,23 @@
-import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
-import thunk from 'redux-thunk';
-import Cookie from 'js-cookie'
-import rootReducer from './user_login/reducers';
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import Cookie from "js-cookie";
+import rootReducer from "./user_login/reducers";
 
+const cartItems = Cookie.getJSON("cartItems") || [];
+const userInfo = Cookie.getJSON("userInfo") || null;
 
-const cartItems=Cookie.getJSON('cartItems') || [];
-const userInfo=Cookie.getJSON('userInfo') || null;
-
-
-const initialState = {cart:{cartItems},userSignIn:{userInfo}};
+const initialState = { cart: { cartItems }, userSignIn: { userInfo } };
 
 const middleWare = [thunk];
 
-
-
 //const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
 const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-        applyMiddleware(...middleWare),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleWare),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
